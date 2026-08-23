f4efaf6a9f156d562e518f68b7c30e209a20cfcb
kanachi0707 <kanachi607@gmail.com>
2026-08-23 12:59:25 +0900

    Update Instagram latest

--- instagram-latest.js
+++ instagram-latest.js
@@ -1,9 +1,9 @@
 ﻿window.__INSTAGRAM_LATEST_POST__ = {
-    "url":  "https://www.instagram.com/p/DcU4ipLBPdr/",
-    "caption":  "#GoodMorningKanachi \n\n誰かに食ってかかることでしか自分を示せないなら\nその言葉が映しているのは相手ではなく、自分の未熟さだ。\n\nIf the only way you can assert yourself is by attacking others, \nyour words reveal not who they are, but how much growing you still have to do.",
-    "updatedAt":  "2026-08-22T12:45:31+09:00",
+    "url":  "https://www.instagram.com/p/DcXjCAIBMls/",
+    "caption":  "#GoodMorningKanachi \n\n自分を客観視できなくなった瞬間、正しさは暴走を始める\n\nWhen self-reflection disappears, \nrighteousness can run out of control.",
+    "updatedAt":  "2026-08-23T12:59:24+09:00",
     "fallbackUrl":  "https://www.instagram.com/p/DVM3FVxErTp/",
     "source":  "instagram-web-profile-api",
     "status":  "ok",
-    "postTakenAt":  "2026-08-22T02:52:44+00:00"
+    "postTakenAt":  "2026-08-23T03:42:33+00:00"
 };
