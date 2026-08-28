5eeeb0bb96a6224dcfb34eca39c4953aa3751670
kanachi0707 <kanachi607@gmail.com>
2026-08-28 13:15:59 +0900

    Update Instagram latest

--- instagram-latest.js
+++ instagram-latest.js
@@ -1,9 +1,9 @@
 ﻿window.__INSTAGRAM_LATEST_POST__ = {
-    "url":  "https://www.instagram.com/p/DchfScJhAK2/",
-    "caption":  "#GoodMorningKanachi \n\n消えていく言葉も\n増えていく言葉も\nどちらも届けたかった想い\n\nSome words fade. \nSome words spread. \nBut every one of them came from something I wanted to share.",
-    "updatedAt":  "2026-08-27T12:00:04+09:00",
+    "url":  "https://www.instagram.com/p/DckTHPThQ27/",
+    "caption":  "#GoodMorningKanachi \n\n知識は広さを\n経験は深さを\n余裕は気づきを生む\n\nKnowledge broadens your view. \nExperience deepens it. \nAnd space within you makes room for insight.",
+    "updatedAt":  "2026-08-28T13:15:58+09:00",
     "fallbackUrl":  "https://www.instagram.com/p/DVM3FVxErTp/",
     "source":  "instagram-web-profile-api",
     "status":  "ok",
-    "postTakenAt":  "2026-08-27T00:22:12+00:00"
+    "postTakenAt":  "2026-08-28T02:33:39+00:00"
 };
